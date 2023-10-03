6abf338975c5d5729d7da0b74b392bb680a33a7a
Lima-49 <vitorlima.soares@hotmail.com>
2023-10-03 16:07:22 -0300

    finalizando cobertura de testes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,46 +1,61 @@
 package org.example;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-public class App 
-{
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class App {
     private String dominio;
+    private String apiURL = "https://app.valimail.com/domain_checker/v1/status/";
+
+    public String getApiURL() {
+        return apiURL;
+    }
+
+    public void setApiURL(String apiURL) {
+        this.apiURL = apiURL;
+    }
+
 
     public void setDominio(String dominio) {
         this.dominio = dominio;
     }
 
-    public void callApi() {
+    public JsonObject callApi() {
 
         try {
-            String apiUrl = "https://app.valimail.com/domain_checker/v1/status/" + dominio + ".json";
+            String apiUrl = apiURL + dominio + ".json";
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl))
+                    .build();
 
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            int responseCode = connection.getResponseCode();
+            if (response.statusCode() == 200) {
+                // Converte a resposta JSON em um objeto JsonObject usando Gson
+                JsonElement jsonElement = JsonParser.parseString(response.body());
+                return jsonElement.getAsJsonObject();
 
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = reader.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                reader.close();
-
-                // Imprima a resposta da API
-                System.out.println("Resposta da API: " + response);
             } else {
-                System.out.println("Erro na chamada da API. Código de resposta: " + responseCode);
+                // Se a resposta não for 200, retorna um objeto JSON com status code e mensagem de erro
+                JsonObject errorObject = new JsonObject();
+                errorObject.addProperty("status_code", response.statusCode());
+                errorObject.addProperty("msg_error", "Erro ao realizar a chamada da API");
+                return errorObject;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException | InterruptedException e) {
+            // Em caso de exceção, retorna um objeto JSON com status code 500 e mensagem de erro
+            JsonObject errorObject = new JsonObject();
+            errorObject.addProperty("status_code", 500);
+            errorObject.addProperty("msg_error", "Erro ao realizar a chamada da API: " + e.getMessage());
+            return errorObject;
         }
     }
+
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,24 +1,43 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import com.google.gson.JsonObject;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
-    App app;
+    private App app;
+
+    @BeforeEach
+    void setUp() {
+        app = new App();
+    }
 
     @Test
-    public void callApiTesteCorreto() {
+    public void callApiTesteSucesso() {
         app = new App();
         app.setDominio("facens.com.br");
-        app.callApi();
+        JsonObject apiResponse = app.callApi();
+        assertTrue(apiResponse.has("domain"));
     }
 
     @Test
-    public void callApiTesteIncorreto() {
+    public void callApiTesteErro() {
         app = new App();
-        app.setDominio("facens");
-        app.callApi();
+        app.setDominio("");
+        JsonObject apiResponse = app.callApi();
+        assertTrue(apiResponse.has("status_code"));
     }
 
+    @Test
+    public void testCallApiWithException() {
+        app = new App();
+        String urlOriginal = app.getApiURL();
+        app.setApiURL("https://invalid-url/");
+        app.setDominio("example.com");
+        JsonObject response = app.callApi();
+        assertEquals(500, response.get("status_code").getAsInt());
+        app.setApiURL(urlOriginal);
+    }
 }
